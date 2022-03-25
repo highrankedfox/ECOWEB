e1ea4578abbee1af340d86ebe5d1aaf049e2d6a4
highrankedfox <78918042+highrankedfox@users.noreply.github.com>
2022-03-25 13:32:17 +0100

    added login form, translation for invalid credentials, remember me option

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -5,16 +5,39 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
 
 class RegisterController extends AbstractController
 {
+    public function __construct(private ManagerRegistry $doctrine) {}
+
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, UserPasswordHasherInterface $hasher): Response
     {
         $user = new User();
+
         $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $user->setIsEnabled(true);
+
+            $password = $hasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $em = $this->doctrine->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
+
         return $this->render('register/index.html.twig', [
             'form' => $form->createView()
         ]);
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,12 +4,16 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
@@ -23,26 +27,34 @@ class RegisterType extends AbstractType
                 ]
             ])
             ->add('pseudo', TextType::class, [
+                'constraints' => new Length(['min' => 2, 'max' => 30]),
                 'label' => 'Pseudo',
                 'attr' => [
                     'placeholder' => 'Choisissez un pseudo'
                 ]
             ])
-            ->add('password', PasswordType::class, [
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe ne coïncident pas.',
                 'label' => 'Mot de passe',
-                'attr' => [
-                    'placeholder' => 'Choisissez un mot de passe'
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Choisissez un mot de passe'
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez le mot de passe'
                 ]
             ])
-            ->add('password_confirm', PasswordType::class, [
+            ->add('termsAccepted', CheckboxType::class, array(
                 'mapped' => false,
-                'label' => 'Confirmation du mot de passe',
-                'attr' => [
-                    'placeholder' => 'Confirmez le mot de passe'
-                ]
-            ])
+                'constraints' => new IsTrue(),
+                'label' => "Merci d'accepter les termes et conditions",
+            ))
             ->add('submit', SubmitType::class, [
                 'label' => "C'est parti !",
+                'attr' => [
+                    'class' => 'btn-success custom-btn rounded-pill'
+                ]
             ])
         ;
     }
